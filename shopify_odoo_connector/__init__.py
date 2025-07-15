74c5209f82d5ab9efbc610e0fa58594ea5fc4355
rashi262025 <rashi262025@gmail.com>
2025-07-15 12:15:32 +0530

    data

--- shopify_odoo_connector/__init__.py
+++
@@ -1,6 +0,0 @@
-# shopify_odoo_connector/__init__.py
-from . import models
-
-# shopify_odoo_connector/models/__init__.py
-# shopify_odoo_connector/controllers/__init__.py
-# (Keep it empty unless you add custom controllers)
--- shopify_odoo_connector/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import config_settings
-from . import shopify_logger
-from . import res_company
-from . import shopify_product
-from . import shopify_customer
-from . import shopify_order
-from . import shopify_order_line
